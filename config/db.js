89fa1344462c8e1beba95ae167587a495bcf3d27
@rahulinfotech2001 <rahulinfotech058@gmail.com>
2024-08-10 20:55:22 +0530

    url changed

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://devtech058:Wvq6iV90RGdr6KJJ@cluster-r.2zp3fam.mongodb.net/', // Replace with your MongoDB URI
+    mongoURI: 'mongodb+srv://devtech058:Wvq6iV90RGdr6KJJ@cluster-r.2zp3fam.mongodb.net/etlhive', // Replace with your MongoDB URI
     jwtSecret: 'etlhive' // Replace with your own secret
 
     // mongodb+srv://devtech058:Wvq6iV90RGdr6KJJ@cluster-r.2zp3fam.mongodb.net/
